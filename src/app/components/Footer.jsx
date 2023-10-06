4ebb41b7f23f17ab77dbcdeeaf1a800abd1a929e
Joboufra <joboufra@gmail.com>
2023-10-06 04:15:29 +0200

    modify footer py

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <footer className='bg-[#121212] py-2 text-center relative bg-opacity-95 border-t-2 border-secondary-800'>
+    <footer className='bg-[#121212] py-4 text-center relative bg-opacity-95 border-t-2 border-secondary-800'>
       <div className='container mx-auto opacity-80'>
         <p className='text-sm text-white relative opacity-70'> Jose Boullosa © {new Date().getFullYear()}</p>
       </div>
