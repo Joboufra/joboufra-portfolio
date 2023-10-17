a6435abece9a9bd46b7cb33307581a73af9848c3
Joboufra <joboufra@gmail.com>
2023-10-17 23:54:45 +0200

    CORS first commit

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,12 +1,25 @@
+import Cors from 'cors';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
+import initMiddleware from './init-middleware';
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['POST'],
+  })
+)
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail =  process.env.FROM_EMAIL;
+const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
+
 export async function POST(req, res) {
+  
+  await cors(req, res);
+
   const body = await req.json();
   const { email, subject, message } = body;
+  
   try {
     const data = await resend.emails.send({
       from: fromEmail,
