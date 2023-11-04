c3d5fcfa1145e0cca7c7d73d025736c9c85f3bbb
Joboufra <joboufra@gmail.com>
2023-11-04 16:52:24 +0100

    Fix: CSS issues in ProjectCard

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import Link from "next/link";
+import Image from 'next/image';
+import { motion } from 'framer-motion'; 
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
-    <div>
-      <div 
-        className="h-52 md:h-72 rounded-t-xl relative group" 
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}>
-          <div className='overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-60 transition-all duration-500'>
-            <Link 
-              href={gitUrl}
-              className='h-14 w-14 mr-2 border-2 relative rounded-full bourder-[#ADB7BE] hover:border-secondary-400 group/link'>
-              <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-secondary-300'/>
+    <div className='text-center'>
+      <div className='relative mx-auto'> 
+        <motion.div className='flex overflow-hidden group'>
+          <div className='overlay items-center justify-center  absolute top-0 left-0 right-0 bottom-0 rounded-t-xl w-full h-full bg-secondary-500 hidden group-hover:flex group-hover:bg-opacity-70 transition-all duration-500 z-10'>
+            <Link href={gitUrl} target="_blank" className='h-10 w-10 border-2 relative rounded-full hover:border-slate-600 group-hover:flex justify-center items-center'>
+                <CodeBracketIcon className='h-6 w-6' />
             </Link>
-            <Link 
-              href={previewUrl}
-              className='h-14 w-14 mr-2 border-2 relative rounded-full bourder-[#ADB7BE] hover:border-secondary-400 group/link'>
-              <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-secondary-300'/>
-            </Link>
+            {previewUrl && (
+              <Link href={previewUrl} target="_blank" className='h-10 w-10 ml-6 border-2 relative rounded-full hover:border-slate-500 group-hover:flex justify-center items-center'>
+                <EyeIcon className='h-6 w-6' />
+              </Link>
+              )}
           </div>
+          <Image 
+            src={imgUrl} 
+            alt={title} 
+            layout='responsive' 
+            width={600} 
+            height={250} 
+            className='rounded-t-xl'
+          />
+        </motion.div>
       </div>
-      <div className='text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4'>
-        <h5 className='text-xl font-semibold mb-2'>{title}</h5>
+      <div className='text-white rounded-b-xl bg-[#181818] py-4 px-4 line-clamp-10'>
+        <h5 className='text-l font-semibold mb-2'>{title}</h5>
         <p className='text-[#ADB7BE]'>{description}</p>
       </div>
     </div>
