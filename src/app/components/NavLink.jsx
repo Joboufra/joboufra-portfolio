21d97f2329c8ee179fbba3f762543e5655180d37
Joboufra <joboufra@gmail.com>
2023-11-04 16:52:38 +0100

    font changes in navlink

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const NavLink = ({ href, title, extraClass, onClose }) => {
   return (
-    <div onClick={onClose} className={`block py-2 px-2 text-[#ADB7BE] md:text-base font-bold rounded hover:text-white ${extraClass}`}>
+    <div onClick={onClose} className={`block py-2 px-2 text-[#ADB7BE] uppercase text-xs font-semibold rounded hover:text-white ${extraClass}`}>
       <Link href={href}>
         {title}
       </Link>
