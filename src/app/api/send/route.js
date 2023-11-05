edb8aedc821b0987cda19f0233a6b48bb2d31be3
Joboufra <joboufra@gmail.com>
2023-11-05 21:38:07 +0100

    Revert CORS changes + set version 1.0.0

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,22 +1,11 @@
-import Cors from 'cors';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import initMiddleware from './init-middleware';
-
-const cors = initMiddleware(
-  Cors({
-    methods: ['POST'],
-  })
-)
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
 
 export async function POST(req, res) {
-  
-  await cors(req, res);
-
   const body = await req.json();
   const { email, subject, message } = body;
   
