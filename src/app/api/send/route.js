dcbfd482e37272bfe51e0e09e87b795895e9af43
Joboufra <joboufra@gmail.com>
2024-01-26 11:02:46 +0100

    revert log changes, CI changes

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -9,10 +9,6 @@ export async function POST(req, res) {
   const body = await req.json();
   const { email, subject, message } = body;
 
-  console.log(process.env.RESEND_API_KEY);
-  console.log(fromEmail);
-  console.log(toEmail);
-
   try {
     const data = await resend.emails.send({
       from: fromEmail,
